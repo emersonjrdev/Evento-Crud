b6bb8c0bfbc4f465d1c84492024ed3f7e08b8544
EmersonJr <152315731+emersonjrdev@users.noreply.github.com>
2024-09-19 15:57:48 -0300

    Subindo att

---
+++ src/router/participante.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+const ParticipantController = require('../controller/participantController');
+const { validateParticipante, validateParticipanteId } = require('../middlewares/participanteValidator');
+
+// Listar todos os participantes
+router.get('/', ParticipantController.getAll);
+
+// Criar um novo participante com validação
+router.post('/', validateParticipante, ParticipantController.create);
+
+// Buscar um participante específico
+router.get('/:id', validateParticipanteId, ParticipantController.getOne);
+
+// Atualizar um participante com validação
+router.put('/:id', validateParticipanteId, validateParticipante, ParticipantController.update);
+
+// Excluir um participante
+router.delete('/:id', validateParticipanteId, ParticipantController.delete);
+
+module.exports = router;
---
+++ src/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const sequelize = require('./config/config');
+
+// Importando as rotas
+const eventoRoutes = require('./router/evento');
+const participanteRoutes = require('./router/participante');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use('/evento', eventoRoutes);
+app.use('/participante', participanteRoutes);
+
+// Sincronizando o banco de dados
+sequelize.sync().then(() => {
+  console.log('Banco de dados sincronizado');
+  app.listen(3000, () => {
+    console.log('Servidor rodando na porta 3000');
+  });
+});
